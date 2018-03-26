0d9ab92d18a677b4aac54dd4e5512327c3d7c906
clm5067 <clm5067@rit.edu>
2018-03-26 12:03:46 -0400

    completed HKTimestamp class functions

--- src/HKTimestamp.java
+++ src/HKTimestamp.java
@@ -1,24 +1,44 @@
 public class HKTimestamp {
     private long timestamp;
+    private String timestamp_bits;
     private int ver;
     private String parent_id_bits;
     private String child_id_bits;
 
     public HKTimestamp(long timestamp){
-        this.timestamp = timestamp;
-        //set variables
-        //String bits = Long.toString(var, 2)
+        try {
+            this.timestamp = timestamp;
+            this.timestamp_bits = Long.toBinaryString(timestamp);
+            this.ver = Integer.parseInt(this.timestamp_bits.substring(this.timestamp_bits.length() - 1));
+            this.parent_id_bits = this.timestamp_bits.substring(this.timestamp_bits.length() - 3);
+
+            if(this.ver == 0) {
+                this.child_id_bits = this.timestamp_bits.substring(this.timestamp_bits.length() - 7, this.timestamp_bits.length() - 4);
+            } else if(this.ver == 1) {
+                this.child_id_bits = this.timestamp_bits.substring(this.timestamp_bits.length() - 9, this.timestamp_bits.length() - 4);
+            }
+        } catch (Exception e) {
+            System.out.println("Error in HKTimestamp constructor: " + e.toString());
+        }
     }
 
-    public getVer() {
-
+    public long getTimestamp() {
+        return this.timestamp;
     }
 
-    public getParentIDBits() {
+    public String getTimestampBits() {
+        return this.timestamp_bits;
+    }
 
+    public int getVer() {
+        return this.ver;
+    }
+
+    public String getParentIDBits() {
+        return this.parent_id_bits;
     }
 
     public String getChildIDBits() {
-        return child_id_bits;
+        return this.child_id_bits;
     }
 }
--- src/HiveKey.java
+++ src/HiveKey.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+
 public class HiveKey {
     private byte[] actual_seed; //SecureRandom.generateSeed(64 bits)
     private int key; //SecureRandom.next(128 bits);
@@ -52,6 +53,14 @@ public class HiveKey {
      * @param args command line arguments, if any
      */
     public static void main(String[] args) {
+        long timestamp = System.currentTimeMillis();
+        HKTimestamp hkt = new HKTimestamp(timestamp);
+
+        System.out.println("Timestamp: " + hkt.getTimestamp());
+        System.out.println("Timestamp bits: " + hkt.getTimestampBits());
+        System.out.println("Version: " + hkt.getVer());
+        System.out.println("Parent ID: " + hkt.getParentIDBits());
+        System.out.println("Child ID: " + hkt.getChildIDBits());
         //get/set timestamp - SYstem.currentTimeMillis()
         //generate 10240 key sequence - output to file
         //delay - random from 0-2 seconds, between each key sequence generated
